f7ddca7fc8c08334ab78a8bdc26ddd39faf2bcd4
IronicGoose <1282318378@qq.com>
2017-07-18 15:44:20 +0800

    主要文件更改

--- MainFiles/Vector.h
+++ MainFiles/Vector.h
@@ -28,10 +28,24 @@ namespace VECTOR {
 	float distance(VECTOR2* a , VECTOR2* b) {
 		return sqrt((a->x - b->x) * (a->x - b->x) + (a->y - b->y)*(a->y - b->y));
 	}
+	float distance2D(VECTOR4* a , VECTOR4* b) {
+		return sqrt((a->x - b->x) * (a->x - b->x) + (a->y - b->y)*(a->y - b->y));
+	}
+	float sqdistance(VECTOR2* a, VECTOR2* b) {
+		return (a->x - b->x) * (a->x - b->x) + (a->y - b->y)*(a->y - b->y);
+	}
+	float sqdistance2D(VECTOR4* a, VECTOR4* b) {
+		return (a->x - b->x) * (a->x - b->x) + (a->y - b->y)*(a->y - b->y);
+	}
 
-	float dot(VECTOR4* a, VECTOR4* b )  { 
+
+	float dot3x3(VECTOR4* a, VECTOR4* b )  { 
 		
 		return a->x *b->x + a->y * b->y + a->z * b->z ;
+	}
+	float dot4x4(VECTOR4* a, VECTOR4* b) {
+
+		return a->x *b->x + a->y * b->y + a->z * b->z + a->w * b->w ;
 	}
 	float matrixdot(VECTOR4* a, VECTOR4* b) { 
 		return a->x *b->x + a->y * b->y + a->z * b->z + a->w * b->w;
@@ -67,14 +81,24 @@ namespace VECTOR {
 		return res;
 
 	}
-	VECTOR4* normalized(VECTOR4*res, VECTOR4* vec) {
-		float abs = dot(vec, vec);
+	VECTOR4* normalizedVector3(VECTOR4*res, VECTOR4* vec) {
+		float abs = dot3x3(vec, vec);
 		abs = sqrt(abs);
 		abs = 1 / abs;
 		res->x = vec->x * abs;
 		res->y = vec->y * abs;
 		res->z = vec->z * abs;
 		return res;
+	}
+	VECTOR4* normalizedVector4(VECTOR4*vec) {
+		float abs = dot4x4(vec, vec);
+		abs = sqrt(abs);
+		abs = 1 / abs;
+		vec->x = vec->x * abs;
+		vec->y = vec->y * abs;
+		vec->z = vec->z * abs;
+		vec->w = vec->w * abs;
+		return vec;
 	}
 }
 namespace MATRIX {
@@ -91,18 +115,15 @@ namespace MATRIX {
 			
 		}  
 	};
-	// 统一使用左乘形式，右乘为特殊使用时再开启 
-	/*
-	VECTOR4*  matrixdot( MATRIX4x4 *matrix, VECTOR4* col) {
-		float x = col->x, y = col->y, z = col->z, w = col->w;
-		VECTOR4 * res = new VECTOR4();
+	// 统一使用左乘形式，右乘为特殊使用时再开启  
+	VECTOR4*  matrixdot( VECTOR4* res ,MATRIX4x4 *matrix, VECTOR4* col) {
+		float x = col->x, y = col->y, z = col->z, w = col->w; 
 		res->x = matrix->val[0][0] * x + matrix->val[0][1] * y + matrix->val[0][2] * z + w * matrix->val[0][3];
 		res->y = matrix->val[1][0] * x + matrix->val[1][1] * y + matrix->val[1][2] * z + w* matrix->val[1][3];
 		res->z = matrix->val[2][0] * x + matrix->val[2][1] * y + matrix->val[2][2] * z + w* matrix->val[2][3];
 		res->w = matrix->val[3][0] * x + matrix->val[3][1] * y + matrix->val[3][2] * z + w * matrix->val[3][3];
 		return res;
-	} 
-	*/
+	}  
 	 
 	VECTOR4*  matrixdot( VECTOR4* res,VECTOR4* col, MATRIX4x4* matrix) {
 		float x = col->x, y = col->y, z = col->z, w = col->w; 
@@ -120,5 +141,34 @@ namespace MATRIX {
 		ma->val[2][2] = 1;
 		ma->val[1][1] = 1;
 		ma->val[0][0] = 1;
+	}
+	void GenerateRotateMatrix(VECTOR4 ax, float angle, MATRIX4x4* ma) {
+		float qr = cos(angle * 3.1415926 / 360);
+		float s = sin(angle * 3.1415926 / 360);  
+		ax.x *= s; ax.y *= s; ax.z *= s; ax.w = qr;
+		
+		normalizedVector4(&ax);
+		ma->val[0][0] = 1 - 2 *(ax.y * ax.y + ax.z * ax.z);
+		ma->val[1][0] = 2 * (ax.x * ax.y - ax.z * ax.w);
+		ma->val[2][0] = 2 * (ax.x * ax.z + ax.y * ax.w);
+		ma->val[0][1] = 2 * (ax.x * ax.y + ax.w * ax.z);
+		ma->val[1][1] = 1 - 2 * (ax.x * ax.x + ax.z * ax.z);
+		ma->val[2][1] = 2 * (ax.y * ax.z - ax.x * ax.w);
+		ma->val[0][2] = 2 * (ax.x * ax.z - ax.y * ax.w);
+		ma->val[1][2] = 2 * (ax.y * ax.z + ax.x * ax.w);
+		ma->val[2][2] = 1 - 2 * (ax.x * ax.x + ax.y * ax.y);
+		ma->val[3][3] = 1;
+		/*
+		ma->val[0][0] = c + ax->x * ax->x * (1 - c);
+		ma->val[0][1] =ax->x * ax->y *(1 - c + ax->z * s);
+		ma->val[0][2] = ax->x * ax->z *(1 - c) - ax->y * s;
+		ma->val[1][0] = ax->x * ax->y * (1 - c) - ax->z * s;
+		ma->val[1][1] = c + ax->y * ax->y * (1 - c);
+		ma->val[1][2] = ax->y * ax->z  * (1 - c) + ax->x  * s;
+		ma->val[2][0] = ax->x * ax->z *(1 - c) + ax->y * s;
+		ma->val[2][1] = ax->y *ax->z *(1 - c) - ax->x *s;
+		ma->val[2][2] = c + ax->z * ax->z *(1 - c); 
+		*/
+
 	}
 }
